5071e68252822ead3347df5a554fc34d0457ca22
Nurtugan Nuraly <nurtugan757@gmail.com>
2020-05-01 19:12:44 +0600

    Initial Commit

---
+++ App Store/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App Store
+//
+//  Created by Nurtugan Nuraly on 5/1/20.
+//  Copyright © 2020 XFamily LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
